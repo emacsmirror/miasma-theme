5e14abb7c1c3b644cc87d09bbfc900afe1dbb3df
daut <bozidardautovic@gmail.com>
2024-10-27 19:00:08 +0100

    feat: rainbow delimiters

--- miasma-theme.el
+++ miasma-theme.el
@@ -88,6 +88,21 @@
    ;;;;; show-paren
    `(show-paren-match ((,class (:background ,miasma-reseda-1 :weight bold))))
 
+   ;;;;; rainbow-delimiters
+   `(rainbow-delimiters-base-face ((,class (:foreground ,miasma-reseda :weight bold))))
+   `(rainbow-delimiters-depth-1-face ((,class (:foreground ,miasma-eucalyptus :weight bold))))
+   `(rainbow-delimiters-depth-2-face ((,class (:foreground ,miasma-walnut :weight bold))))
+   `(rainbow-delimiters-depth-3-face ((,class (:foreground ,miasma-lemon :weight bold))))
+   `(rainbow-delimiters-depth-4-face ((,class (:foreground ,miasma-brass :weight bold))))
+   `(rainbow-delimiters-depth-5-face ((,class (:foreground ,miasma-olive :weight bold))))
+   `(rainbow-delimiters-depth-6-face ((,class (:foreground ,miasma-copper :weight bold))))
+   `(rainbow-delimiters-depth-7-face ((,class (:foreground ,miasma-reseda :weight bold))))
+   `(rainbow-delimiters-depth-8-face ((,class (:foreground ,miasma-cedar :weight bold))))
+   `(rainbow-delimiters-depth-9-face ((,class (:foreground ,miasma-lemon :weight bold))))
+   `(rainbow-delimiters-depth-10-face ((,class (:foreground ,miasma-ecru :weight bold))))
+   `(rainbow-delimiters-depth-11-face ((,class (:foreground ,miasma-tangerine :weight bold))))
+   `(rainbow-delimiters-depth-11-face ((,class (:foreground ,miasma-obsidian :weight bold))))
+
    ;;;;; persp-mode
    `(persp-selected-face ((,class (:foreground ,miasma-lemon))))
 
