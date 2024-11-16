f049395300dd3590b22918c8288afacd1f89da50
daut <bozidardautovic@gmail.com>
2024-11-17 00:24:34 +0100

    chore: add version to commentary

--- miasma-theme.el
+++ miasma-theme.el
@@ -2,6 +2,7 @@
 
 ;; Author: Bozidar Dautovic
 ;; URL: http://github.com/daut/miasma-theme.el
+;; Version: 1.0.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
